0f0be8e9c9c12299fb0f0ded9573bda8f3e86757
Kathryn Lawton <klawton16@gmail.com>
2018-06-14 11:20:07 -0500

    updating test class

---
+++ CustomListTest/CustomListTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTest
+{
+	[TestClass]
+	public class CustomListTest
+	{
+		[TestMethod]
+		public void Add_FirstValue_ZeroIndex()
+		{
+			List<int> customList = new List<int>();
+			int firstValue = 1;
+
+			customList.Add(firstValue);
+
+			Assert.AreEqual(firstValue, customList[0]);
+		}
+
+		[TestMethod]
+		public void Add_SecondValue_FirstIndex()
+		{
+			List<int> customList = new List<int>();
+			int secondValue = 2;
+
+			customList.Add(secondValue);
+
+			Assert.AreEqual(secondValue, customList[1]);
+		}
+	}
+}
